0bac0e4c963bf47baa232b29213e2313cf0e94f5
dhanushsrs <dhanushsrs@gmail.com>
2024-08-26 16:03:15 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const mailRoutes = require('./routes/mailRoutes');
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Frontend URL
+    origin: 'https://reactjs-reachinbox-assignment-reachinbox-ddsg.onrender.com', // Frontend URL
     methods: 'GET,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization' // Allow Authorization header
 }));
