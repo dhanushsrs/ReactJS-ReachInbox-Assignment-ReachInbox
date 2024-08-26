8b9fb7d372e933564404d604ec5ae11ec116845b
dhanushsrs <dhanushsrs@gmail.com>
2024-08-26 16:07:24 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -91,7 +91,7 @@ const Login = () => {
 
     // Function to handle Google login
     const handleLogin = () => {
-        window.location.href = 'https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reactjs-reachinbox-assignment-reachinbox-ddsg.onrender.com';
+        window.location.href = 'https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000';
     };
 
     // Handle token if present in the URL after login redirect
